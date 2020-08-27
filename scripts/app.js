835a7e68e6b7c483d94ca724d9b5f660e509fca0
dongra02 <dsgraham1@gmail.com>
2020-08-27 14:30:37 +0300

    Layout Grid, Layout Mines, Check Individual Cell Neighbors Complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,15 @@
 
 
+
+
 function init () {
   console.log('JS is working.')
 
   const cells = []
+  const mines = []
   const height = 20
   const width = 20
-  const mineCount = 10
+  const mineCount = 40
   const cellCount = height * width
 
   const grid = document.querySelector('.grid')
@@ -30,12 +33,41 @@ function init () {
         i--
       } else {
         cells[randCellNum].classList.add('mine')
+        mines.push(cells[randCellNum])
       }
     }
   }
 
-  createGrid()
+  
 
+  // function to check neighbors for mine class
+  // recurse on each neighbor cell (not mine)
+
+  function checkMines (cell) {
+    console.log(cell)
+    const up = cell - width
+    const upLeft = up - 1
+    const upRight = up + 1
+    const right = cell + 1
+    const left = cell - 1
+    const down = cell + width
+    const downLeft = down - 1
+    const downRight = down + 1
+    const neighbors = [up, upLeft, upRight, right, left, down, downLeft, downRight].filter(neighbor => !cells[neighbor] === false)
+    let counter = 0
+    console.log(neighbors)
+    neighbors.forEach(neighbor => {
+      if (cells[neighbor].classList.contains('mine')) {
+        counter++
+      }
+    })
+    cells[cell].innerHTML = counter
+  }
+
+  createGrid()
+  checkMines(6)
+
+  
 
 
 
